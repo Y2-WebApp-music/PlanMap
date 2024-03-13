cb2025eea0fd6530877f389490a7e76e05c9d0b5
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-03-13 14:48:12 +0700

    🌱 Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import '../global.css'
+import './navbar.css'
 
 function Navbar(){
-
+    return(
+        <>
+        <div className="NavBar">
+            <p>เที่ยวหนายยย</p>
+            <div></div>
+            <div className="link-btn">
+                <input type="button" value="แพลนของฉัน" />
+                <input type="button" value="สร้างแพลนใหม่" />
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import './home.css'
 import '../global.css'
+import Navbar from "../components/Navbar";
 
 function Home(){
     return(
         <>
+        <Navbar />
         <div className="MainTitle">
-            <h1>เที่ยวนายยย</h1>
+            <h1>เที่ยวหนายยย</h1>
             <h3>เว็บจัดการวางแผนการเดินทาง</h3>
             <input type="button" value="เริ่มสร้างแพลน" />
+            <div className="gr-color"> <p></p></div>
         </div>
+        <div> <p>textxjkgnb;lxfmnl;xfmgn;lxfgn xfgl;nmxl;fgnm</p></div>
         </>
     )
 }
