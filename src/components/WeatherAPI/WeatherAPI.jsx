61d4f2360a806fd205d654e643f6b06cd9890f3d
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-03-18 01:44:08 +0700

    🌱 WeatherAPI File

---
+++ src/components/WeatherAPI/WeatherAPI.jsx
@@ -0,0 +1,20 @@
+import React, { useContext, useState, useEffect } from "react";
+
+const api = {
+    key: "f691e7c3239c23d19b0ee9ae9972f6b8",
+    Base: "https://home.openweathermap.org/data/2.5/",
+};
+
+function WeatherAPI(){
+
+    const [search, setSearch] = useState("")
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default WeatherAPI
+
