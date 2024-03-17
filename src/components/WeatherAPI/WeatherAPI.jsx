b85a0e6cd9b7f61fa105ce6e39f269dedfac3f6c
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-03-18 01:45:50 +0700

    💄minimal update

--- src/components/WeatherAPI/WeatherAPI.jsx
+++ src/components/WeatherAPI/WeatherAPI.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 
+// for Free plan of API can forecast 5 Days
+// We will use forecast 5 Days to give some example for this API
 const api = {
     key: "f691e7c3239c23d19b0ee9ae9972f6b8",
     Base: "https://home.openweathermap.org/data/2.5/",
