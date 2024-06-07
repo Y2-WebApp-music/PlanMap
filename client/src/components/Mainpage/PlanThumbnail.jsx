c4291a9581f7787367338733dee76b8feb9a28d2
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-06-08 01:00:07 +0700

    💄 SearchPlan

--- client/src/components/Mainpage/PlanThumbnail.jsx
+++ client/src/components/Mainpage/PlanThumbnail.jsx
@@ -1,16 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import '/src/global.css'
 import './planthumbnail.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faEllipsisVertical, faPencil, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { formatThaiDate } from "../DateFormat";
 
-function Thumbnail({uid, handleOrderSelection, planList, clickedButton }){
+function Thumbnail({uid, handleOrderSelection, planList, clickedButton, setPlanList }){
     const [searchQuery, setSearchQuery] = useState('');
+    const [searching,setSearching] = useState(false)
 
     const handleSearchInputChange = (event) => {
         setSearchQuery(event.target.value);
+        if (searching === true){
+            if (event.target.value === '') {
+                fetchPlan()
+                setSearching(false)
+            }
+        }
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSearch()
+        }
+    };
+
+    const handleSearch = async(e)=>{
+        setSearching(true)
+        try {
+            await fetch(`http://localhost:3000/search?uid=${uid}&string=${searchQuery}`, {
+                method: 'POST'
+            }).then(response => response.json())
+            .then(data => setPlanList(data))
+            .catch(error => console.error('Error fetching search results:', error));
+        } catch (error) {
+            console.error("Error send post:", error);
+            throw error;
+        }
+    }
+
+    const fetchPlan = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/mainpage?uid=${uid}&planOrder=-1`);
+            const plan = await response.json();
+            if (Object.keys(plan).length === 0 && plan.constructor === Object) {
+                console.log("Plan is empty. Retrying...");
+                retryCount++;
+                if (retryCount <= maxRetries) {
+                    setTimeout(fetchPlan, 1000);
+                } else {
+                    console.log("Max retries exceeded. Unable to fetch plan.");
+                }
+            } else {
+                setPlanList(plan);
+            }
+        } catch (error) {
+            console.error('Error fetching plan:', error);
+            setTimeout(fetchPlan, 1000);
+        }
     };
 
     return(<>
@@ -18,8 +67,8 @@ function Thumbnail({uid, handleOrderSelection, planList, clickedButton }){
         <div className="FilterArea">
             <div className="searchBox">
                 <label htmlFor="search" id="searchLabel">
-                    <input type="text" placeholder="ค้นหาแพลน" id="search" value={searchQuery} onChange={handleSearchInputChange} />
-                    <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" style={{ color: 'var(--color-text)' }}/>
+                    <input type="text" placeholder="ค้นหาแพลน" id="search" value={searchQuery} onChange={handleSearchInputChange} onKeyDown={handleKeyDown}/>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" id="searchIconPlan" onClick={handleSearch}/>
                 </label>
             </div>
             <div className="BtnZone">
